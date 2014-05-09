0995b44582228ddb928adfb6e7031f50d24b6e43
Nick Husher <nick.husher@dealer.com>
2014-05-09 16:41:47 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject om-rte "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.5.0"]
+                 [hickory "0.5.3"]]
+
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {
+    :builds [{:id "om-rte"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "om_rte.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
